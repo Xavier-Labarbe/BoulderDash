f0c87f69d9e7a13674b729a4edbed94b4f7c7f4e
matheoberger <matheo.berger@viacesi.fr>
2019-05-31 18:04:02 +0200

    Fix PlayableMap to merge

--- model/src/main/java/model/PlayableMap.java
+++ model/src/main/java/model/PlayableMap.java
@@ -2,46 +2,151 @@ package model;
 
 import java.util.ArrayList;
 
+import contract.IElement;
+import contract.IExit;
+import contract.IMobile;
+import contract.IPlayableMap;
+import contract.IPlayer;
 import model.element.Element;
-import model.element.mobile.Mobile;
 
-public class PlayableMap {
-    private Element[][] elements;
-    private final ArrayList<Mobile> mobiles;
+public class PlayableMap implements IPlayableMap {
+    private IElement[][] elements;
+    private final ArrayList<IMobile> mobiles;
+    private final ArrayList<IMobile> waitingMobilesForCreation;
+    private final ArrayList<IMobile> waitingMobilesForRemoving;
+    private IPlayer player;
+    private IExit exit;
+    private Boolean win = false;
+
+    private int numberOfDiamondForWin;
+
+    private int width;
+
+    private int height;
 
     private String label;
 
-    public PlayableMap() {
+    public PlayableMap(final int numberOfDiamondForWin) {
         this.elements = new Element[20][20];
-        this.mobiles = new ArrayList<Mobile>();
+        this.mobiles = new ArrayList<IMobile>();
+        this.waitingMobilesForCreation = new ArrayList<IMobile>();
+        this.waitingMobilesForRemoving = new ArrayList<IMobile>();
+        this.setNumberOfDiamondForWin(numberOfDiamondForWin);
     }
 
-    public void addMobiles(final Mobile mobile) {
+    @Override
+    public void addMobiles(final IMobile mobile) {
         this.mobiles.add(mobile);
     }
 
-    public Element[][] getElements() {
+    @Override
+    public void addwaitingMobilesForCreation(final IMobile mobile) {
+        this.waitingMobilesForCreation.add(mobile);
+    }
+
+    @Override
+    public void addwaitingMobilesForRemoving(final IMobile mobile) {
+        this.waitingMobilesForRemoving.add(mobile);
+    }
+
+    @Override
+    public IElement[][] getElements() {
         return this.elements;
 
     }
 
-    public ArrayList<Mobile> getMobiles() {
+    @Override
+    public IExit getExit() {
+        return this.exit;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public ArrayList<IMobile> getMobiles() {
         return this.mobiles;
     }
 
-    public Element getXYElement(final int x, final int y) {
+    @Override
+    public int getNumberOfDiamondForWin() {
+        return this.numberOfDiamondForWin;
+    }
+
+    @Override
+    public IPlayer getPlayer() {
+        return this.player;
+    }
+
+    @Override
+    public ArrayList<IMobile> getWaitingMobilesForCreation() {
+        return this.waitingMobilesForCreation;
+    }
+
+    @Override
+    public ArrayList<IMobile> getWaitingMobilesForRemoving() {
+        return this.waitingMobilesForRemoving;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.width;
+    }
+
+    @Override
+    public Boolean getWin() {
+        return this.win;
+    }
+
+    @Override
+    public IElement getXYElement(final int x, final int y) {
         return this.elements[x][y];
     }
 
-    public void removeMobiles(final Mobile mobile) {
+    @Override
+    public void removeMobiles(final IMobile mobile) {
         this.mobiles.remove(mobile);
     }
 
-    public void setElements(final Element[][] elements) {
+    @Override
+    public void setElements(final IElement[][] elements) {
         this.elements = elements;
     }
 
-    public void setXYElement(final int x, final int y, final Element element) {
+    @Override
+    public void setExit(final IExit exit) {
+        this.exit = exit;
+    }
+
+    @Override
+    public void setHeight(final int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void setNumberOfDiamondForWin(final int numberOfDiamondForWin) {
+        this.numberOfDiamondForWin = numberOfDiamondForWin;
+    }
+
+    @Override
+    public void setPlayer(final IPlayer player) {
+        this.player = player;
+    }
+
+    @Override
+    public void setWidth(final int width) {
+        this.width = width;
+    }
+
+    @Override
+    public void setWin(final Boolean win) {
+        this.win = win;
+    }
+
+    @Override
+    public void setXYElement(final int x, final int y, final IElement element) {
         this.elements[x][y] = element;
     }
 }
