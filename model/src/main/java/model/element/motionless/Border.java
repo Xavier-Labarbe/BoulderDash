f6f566421f206b49b2ec32d29f16a0c2df309296
matheoberger <matheo.berger@viacesi.fr>
2019-06-01 01:38:11 +0200

    Fix Border to merge
    Sprite variable '|' type changed from char to String "|"

--- model/src/main/java/model/element/motionless/Border.java
+++ model/src/main/java/model/element/motionless/Border.java
@@ -1,11 +1,11 @@
 package model.element.motionless;
 
-import contract.Permeability;
 import contract.ISprite;
+import contract.Permeability;
 import model.element.Sprite;
 
 public class Border extends Motionless {
-    private static ISprite sprite = new Sprite('|', "Border.jpg");
+    private static ISprite sprite = new Sprite("|", "Border.jpg");
     private static Boolean breakable = false;
     private static Permeability premeability = Permeability.BLOCKING;
     private static Boolean explosable = false;
