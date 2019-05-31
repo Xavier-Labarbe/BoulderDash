ec662d3245d4d4840b1058b6eb2eb65c2fa9c27c
matheoberger <matheo.berger@viacesi.fr>
2019-06-01 01:26:26 +0200

    Fix Diamond to merge

--- model/src/main/java/model/element/mobile/Diamond.java
+++ model/src/main/java/model/element/mobile/Diamond.java
@@ -1,20 +1,23 @@
 package model.element.mobile;
 
+import contract.IDiamond;
+import contract.IPlayableMap;
+import contract.ISprite;
 import contract.Permeability;
-import contract.iSprite;
-import model.PlayableMap;
 import model.element.Sprite;
 
-public class Diamond extends FallingMobile {
-    private static iSprite SPRITE = new Sprite("D", "Diamond.jpg");
-    private static Permeability PERMEABILITY = Permeability.BLOCKING;
-    private static Boolean EXPLOSABLE = true;
-    private static StrategyMove STRATEGY_MOVE = new MoveGravity();
+public class Diamond extends FallingMobile implements IDiamond {
+    private static ISprite sprite = new Sprite("D", "Diamond.jpg");
+    private static Permeability permeability = Permeability.BLOCKING;
+    private static Boolean explosable = true;
 
-    public Diamond(final int x, final int y, final PlayableMap map) {
-        super(SPRITE, PERMEABILITY, EXPLOSABLE, x, y, STRATEGY_MOVE);
-        this.setMap(map);
+    public Diamond(final int x, final int y, final IPlayableMap map) {
+        super(sprite, permeability, explosable, x, y, map);
+        this.getMap().setXYElement(x, y, this);
         this.getMap().addMobiles(this);
     }
 
+    public Diamond(final int x, final int y, final IPlayableMap map, final int nullParamater) {
+        super(sprite, permeability, explosable, x, y, map);
+    }
 }
