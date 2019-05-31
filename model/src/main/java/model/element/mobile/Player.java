aec622758071cbd78beb5b0e5f2f826362c1e031
matheoberger <matheo.berger@viacesi.fr>
2019-06-01 01:36:00 +0200

    fix Player to merge

--- model/src/main/java/model/element/mobile/Player.java
+++ model/src/main/java/model/element/mobile/Player.java
@@ -1,55 +1,89 @@
 package model.element.mobile;
 
+import contract.ControllerOrder;
+import contract.IDiamond;
+import contract.IPlayableMap;
+import contract.IPlayer;
+import contract.IRock;
+import contract.ISprite;
 import contract.Permeability;
-import contract.iSprite;
-import model.PlayableMap;
 import model.element.Sprite;
 import model.element.motionless.Tunnel;
 
-public class Player extends AliveMobile {
-    private static iSprite SPRITE = new Sprite("P", "Player.jpg");
-    private static Permeability PERMEABILITY = Permeability.PENETRABLE;
-    private static Boolean EXPLOSABLE = true;
-    private static StrategyMove STRATEGY_MOVE = new MoveInput();
+public class Player extends AliveMobile implements IPlayer {
+    private static ISprite sprite = new Sprite("P", "Player.jpg");
+    private static Permeability permeability = Permeability.PENETRABLE;
+    private static Boolean explosable = true;
+    private static StrategyMove strategyMove = new MoveInput();
+    private int numberOfTakenDiamond = 0;
+    private ControllerOrder movingOrder = ControllerOrder.NOTHING;
 
-    public Player(final int x, final int y, final PlayableMap map) {
-        super(SPRITE, PERMEABILITY, EXPLOSABLE, x, y, STRATEGY_MOVE);
-        this.setMap(map);
+    public Player(final int x, final int y, final IPlayableMap map) {
+        super(sprite, permeability, explosable, x, y, strategyMove, map);
+        this.getMap().setXYElement(x, y, this);
+        this.getMap().setPlayer(this);
         this.getMap().addMobiles(this);
     }
 
+    @Override
     public void digTunnel(final int x, final int y) {
         this.getMap().setXYElement(x, y, new Tunnel());
     }
 
-    public void pickupDiamond(final Diamond diamond) {
-        diamond.setVisible(false);
-        diamond.getMap().removeMobiles(diamond);
+    @Override
+    public ControllerOrder getMovingOrder() {
+        return this.movingOrder;
     }
 
-    public void pushRock(final Rock rock) {
-        final int x_p = this.getX();
-        final int y_p = this.getY();
-        final int x_r = rock.getX();
-        final int y_r = rock.getY();
-        if ((x_r == (x_p + 1)) && (y_r == y_p)) {
-            System.out.println("droite");
-            rock.setX(x_r + 1);
-        } else if ((x_r == (x_p - 1)) && (y_r == y_p)) {
-            System.out.println("gauche");
-            rock.setX(x_r - 1);
-        } else if ((y_r == (y_p - 1)) && (x_r == x_p)) {
-            System.out.println("haut");
-            rock.setY(y_r - 1);
-        } else if ((y_r == (y_p + 1)) && (x_r == x_p)) {
-            System.out.println("bas");
-            rock.setY(y_r + 1);
+    @Override
+    public int getNumberOfTakenDiamond() {
+        return this.numberOfTakenDiamond;
+    }
+
+    @Override
+    public void pickupDiamond(final IDiamond diamond) {
+        this.getMap().addwaitingMobilesForRemoving(diamond);
+        this.setNumberOfTakenDiamond(this.getNumberOfTakenDiamond() + 1);
+        if (this.getNumberOfTakenDiamond() == this.getMap().getNumberOfDiamondForWin()) {
+            this.getMap().getExit().open();
+        }
+
+    }
+
+    @Override
+    public void pushRock(final IRock rock) {
+        final int xPlayer = this.getX();
+        final int yPlayer = this.getY();
+        final int xRock = rock.getX();
+        final int yRock = rock.getY();
+        if ((xRock == (xPlayer + 1)) && (yRock == yPlayer)) {
+            this.verifyAndPushForRockPushing(xRock + 1, yRock, rock);
+        } else if ((xRock == (xPlayer - 1)) && (yRock == yPlayer)) {
+            this.verifyAndPushForRockPushing(xRock - 1, yRock, rock);
+        } else if ((yRock == (yPlayer - 1)) && (xRock == xPlayer)) {
+            this.verifyAndPushForRockPushing(xRock, yRock - 1, rock);
+        } else if ((yRock == (yPlayer + 1)) && (xRock == xPlayer)) {
+            this.verifyAndPushForRockPushing(xRock, yRock + 1, rock);
         }
     }
 
     @Override
-    public void setAlive(final Boolean alive) {
-        this.alive = alive;
+    public void setMovingOrder(final ControllerOrder movingOrder) {
+        this.movingOrder = movingOrder;
+    }
+
+    @Override
+    public void setNumberOfTakenDiamond(final int numberOfTakenDiamond) {
+        this.numberOfTakenDiamond = numberOfTakenDiamond;
+    }
+
+    private void verifyAndPushForRockPushing(final int x, final int y, final IRock rock) {
+        if (this.getMap().getXYElement(x, y).getPermeability() == Permeability.PENETRABLE) {
+            this.getMap().setXYElement(rock.getX(), rock.getY(), new Tunnel());
+            rock.setX(x);
+            rock.setY(y);
+            this.getMap().setXYElement(x, y, rock);
+        }
     }
 
 }
