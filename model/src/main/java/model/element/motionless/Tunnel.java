d21eabd709fe3d9938216d9fdf398c5e955bbbfa
matheoberger <matheo.berger@viacesi.fr>
2019-06-01 01:55:50 +0200

    Fix Tunnel to merge
    Sprite variable 'T' type change from char to String "T"

--- model/src/main/java/model/element/motionless/Tunnel.java
+++ model/src/main/java/model/element/motionless/Tunnel.java
@@ -1,11 +1,11 @@
 package model.element.motionless;
 
-import contract.Permeability;
 import contract.ISprite;
+import contract.Permeability;
 import model.element.Sprite;
 
 public class Tunnel extends Motionless {
-    private static ISprite sprite = new Sprite('T', "Tunnel.jpg");
+    private static ISprite sprite = new Sprite("T", "Tunnel.jpg");
     private static Boolean breakable = false;
     private static Permeability premeability = Permeability.PENETRABLE;
     private static Boolean explosable = true;
