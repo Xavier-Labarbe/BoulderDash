0990aa600030d4abe846ad084b6173e84a7ddc3a
matheoberger <matheo.berger@viacesi.fr>
2019-06-01 01:52:07 +0200

    Fix ExplosableWall to merge
    Sprite variable 'W' type change from char to String "W"

--- model/src/main/java/model/element/motionless/ExplosableWall.java
+++ model/src/main/java/model/element/motionless/ExplosableWall.java
@@ -1,11 +1,11 @@
 package model.element.motionless;
 
-import contract.Permeability;
 import contract.ISprite;
+import contract.Permeability;
 import model.element.Sprite;
 
 public class ExplosableWall extends Motionless {
-    private static ISprite sprite = new Sprite('W', "ExplosableWall.jpg");
+    private static ISprite sprite = new Sprite("W", "ExplosableWall.jpg");
     private static Boolean breakable = false;
     private static Permeability premeability = Permeability.BLOCKING;
     private static Boolean explosable = true;
