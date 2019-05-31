0fe87133989f50f0f15c9b87ab4f1e2a684e51f6
Brosset-Axel <44434284+Brosset-Axel@users.noreply.github.com>
2019-05-31 16:20:38 +0200

    fix factory

--- model/src/main/java/model/element/mobile/MobileFactory.java
+++ model/src/main/java/model/element/mobile/MobileFactory.java
@@ -9,28 +9,27 @@ import contract.IRock;
 public class MobileFactory {
     static int x = 0;
     static int y = 0;
-    static IPlayableMap playableMap;
-    private static IDiamond diamond = new Diamond(x, y, playableMap);
-    private static Monster monster = new Monster(x, y, playableMap);
-    private static IRock rock = new Rock(x, y, playableMap);
-    private static IPlayer player = new Player(x, y, playableMap);
-    private static IMobile[] mobile = { diamond, monster, rock, player, };
 
-    public static IMobile getFromFileSymbol(final char fileSymbol, final int x, final int y,
-            final IPlayableMap playableMap) {
-        for (final IMobile mobile : mobile) {
-            if (mobile.getSprite().getConsoleImage() == fileSymbol) {
-                mobile.setX(x);
-                mobile.setY(y);
-                mobile.setMap(playableMap);
-                return mobile;
-            }
-        }
-        return null;
-    }
+    private final IMobile[] mobile = new IMobile[4];
+
+    private final IDiamond diamond;
+
+    private final Monster monster;
+    private final IRock rock;
+    private final IPlayer player;
+
+    private IPlayableMap playableMap = null;
 
     public MobileFactory(final IPlayableMap map) {
-        MobileFactory.playableMap = map;
+        this.playableMap = map;
+        this.diamond = new Diamond(x, y, this.playableMap);
+        this.mobile[0] = this.diamond;
+        this.monster = new Monster(x, y, this.playableMap);
+        this.mobile[1] = this.diamond;
+        this.rock = new Rock(x, y, this.playableMap);
+        this.mobile[2] = this.diamond;
+        this.player = new Player(x, y, this.playableMap);
+        this.mobile[3] = this.diamond;
     }
 
     public IMobile createDiamond(final int x, final int y, final IPlayableMap playableMap) {
@@ -47,5 +46,17 @@ public class MobileFactory {
 
     public IMobile createRock(final int x, final int y, final IPlayableMap playableMap) {
         return new Rock(x, y, playableMap);
+    }
+
+    public IMobile getFromFileSymbol(final char fileSymbol, final int x, final int y, final IPlayableMap playableMap) {
+        for (final IMobile mobile : this.mobile) {
+            if (mobile.getSprite().getConsoleImage() == fileSymbol) {
+                mobile.setX(x);
+                mobile.setY(y);
+                mobile.setMap(playableMap);
+                return mobile;
+            }
+        }
+        return null;
     }
 }
